160d754172982095a00d4729956c657055487ee6
kirshach <kirshach@mail.ru>
2022-08-02 02:14:55 +0400

    update types

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,23 @@
+export type Axis = 'x' | 'y' | 'z';
+
+export type NodeDirection = '-x' | '+x' | '-y' | '+y' | '-z' | '+z';
+
+export interface Dimensions {
+  width: number;
+  height: number;
+  depth: number;
+};
+
+export type Position = {
+  [Key in Axis]: number;
+};
+
 export interface Element {
-  id: string,
-  width: number,
-  height: number,
-  depth: number,
-  padding?: number,
+  dimensions: Dimensions;
+  id: string;
+  padding?: number;
 };
 
 export interface PositionedElement extends Element {
-  x: number,
-  y: number,
-  z: number,
+  position: Position,
 };
